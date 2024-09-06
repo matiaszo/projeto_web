bf3bb96af397acbcb2a377e62fc8e2e47ee46f63
milena579 <milecalegari@gmail.com>
2024-09-06 10:58:38 -0300

    cancelamento de reservas finalizado

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,31 @@
 
-const dataReserva = document.getElementById('dataReserva');
-
-dataReserva.min = Date.now();
+// const dataReserva = document.getElementById('dataReserva');
+// dataReserva.min = Date.now();
 
 function mudarCapacidade(){
     let local = document.getElementById('localReserva').value;
     local = local.split('/');
     let quant = document.getElementById('quantidade');
     quant.value  = local[1];
+}
+
+
+function cancelaReserva(idReservaLocal){
+    let local = document.getElementById('Local'+ idReservaLocal ).innerHTML;
+    let capacidade = document.getElementById('Capacidade' + idReservaLocal).innerHTML;
+    let data = document.getElementById('Data' + idReservaLocal).innerHTML;
+    let horario = document.getElementById('Hora' + idReservaLocal).innerHTML;
+
+    let localModal = document.getElementById('LocalModal');
+    let capacidadeModal = document.getElementById('CapModal');
+    let dataModal = document.getElementById('DataModal');
+    let horaModal = document.getElementById('HoraModal');
+
+    localModal.innerHTML = local;
+    capacidadeModal.innerHTML = capacidade;
+    dataModal.innerHTML = data;
+    horaModal.innerHTML = horario;
+
+    let form = document.getElementById('cancelaReserva');
+    form.action = '/excluirReservaLocal/' + idReservaLocal
 }
