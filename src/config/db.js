2070c17755b8ff7badbd8294c44fbda83321c3d2
milena579 <milecalegari@gmail.com>
2024-09-10 09:20:45 -0300

    colocando required

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const sequelize = require('sequelize');
 //configurações da base de dados
 const database = new sequelize('projeto_web', 'usuario', '123',
 {
-    dialect: 'mssql', host:'localhost', port: 62470
+    dialect: 'mssql', host:'localhost', port: 1433
 });
 database.sync();
 module.exports = database;
