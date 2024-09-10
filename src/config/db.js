e7125f36b0184366442fead098e0e51a57299b21
matiaszo <tiaszuniga@gmail.com>
2024-09-10 11:47:42 -0300

    bug do filtro das reservas arrumado

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const sequelize = require('sequelize');
 //configurações da base de dados
-const database = new sequelize('projeto_web', 'usuario', '123',
+const database = new sequelize('projeto_web', 'projeto_web', '123',
 {
     dialect: 'mssql', host:'localhost', port: 1433
 });
